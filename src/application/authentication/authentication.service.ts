b272ca6b889be1c9245143b3cddc3f7b772ebf4d
Davi Seagull <davi.seagull@gmail.com>
2023-09-21 15:46:10 -0300

    fix: import in authentication.service

--- src/application/authentication/authentication.service.ts
+++ src/application/authentication/authentication.service.ts
@@ -1,4 +1,4 @@
-import { SignUpRequest } from '@/application/use-cases/sign-up/sign-up.use-case'
+import { SignUpRequest } from '../use-cases/auth/sign-up/sign-up.use-case'
 
 export interface AuthenticationResult {
   status: string
